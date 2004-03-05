514058a8a347444f978cd01325cb84e525a63a09
Martyn Plummer <plummer@iarc.fr>
2004-03-05 00:00:00 +0000

    version 1.9-2

--- R/gtk.R
+++ R/gtk.R
@@ -1,7 +1,7 @@
 gtk <- function(display = "", width = 7, height = 7, pointsize = 12)
 {
     .C("do_GTK", as.character(display), as.numeric(width),
-       as.numeric(height), as.numeric(pointsize))
+       as.numeric(height), as.numeric(pointsize), PACKAGE="gtkDevice")
     return(invisible(TRUE))
 }
 
@@ -9,13 +9,14 @@ GTK <- gtk
 
 asGtkDevice <- function(widget, width = 300, height = 300, pointsize = 12)
 {
-    require("RGtk")
+    if (!require("RGtk"))
+       stop("asGtkDevice requires the RGtk package")
     if(!inherits(widget, "GtkDrawingArea")) {
         stop("Widget being used as a Gtk Device must be or extend the GtkDrawingWidget class")
     }
 
     .C("do_asGTKDevice", widget, as.numeric(width), as.numeric(height),
-       as.numeric(pointsize))
+       as.numeric(pointsize), PACKAGE="gtkDevice")
 }
 
 
